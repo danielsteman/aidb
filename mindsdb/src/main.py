1343972c37834a849b465accc10a80fd9721ae96
danielsteman <daniel-steman@live.nl>
2023-02-11 23:45:44 +0100

    added db client

---
+++ mindsdb/src/main.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+
+user = 'mindsdb'
+password = ''
+host = '127.0.0.1'
+port = 47335
+database = ''
+
+def get_connection():
+    return create_engine(
+        url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(user, password, host, port, database)
+    )
+
+if __name__ == '__main__':
+    try:
+        engine = get_connection()
+        engine.connect()
+        print(f"Connection to the {host} for user {user} created successfully.")
+    except Exception as ex:
+        print("Connection could not be made due to the following error: \n", ex)
